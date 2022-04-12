a8475db46fa13fa1cce7ef98f002fadd3ca42bab
nahlavalentina <nahlavalentinas@gmail.com>
2022-04-12 15:00:13 -0300

    converti código para jquery

--- js/recebeApod.js
+++ js/recebeApod.js
@@ -1,25 +1,39 @@
-const requisicao = new XMLHttpRequest();
+// const requisicao = new XMLHttpRequest();
 const url = "https://api.nasa.gov/planetary/apod?date="
 const apiKey = "&api_key=f5EySbgzwGEYPyNUCE5AM0MKzNo0CZHAOwhhsgBd"
-const dateInput = document.querySelector("#user-date");
-const btn = document.querySelector("#submit-btn");
+const dateInput = $("#user-date");
 
-btn.addEventListener('click', function(event){
+$("#submit-btn").click(function (event) {
   event.preventDefault();
-  requisicao.open("GET", url + dateInput.value + apiKey);
+  $.ajax({
+    url: `${url + dateInput.val() + apiKey}`,
+    success: function (resposta) {
+      $("#result-date").text(resposta.date);
+      $("#title").text(resposta.title);
+      $("#result-img").attr("src", `${resposta.hdurl}`);
+      $("#explanation").text(resposta.explanation);
+    },
+  });
+});
 
-  requisicao.onload = function () {
-    if (requisicao.status == 200) {
-      const respostaReq = JSON.parse(requisicao.responseText);
-      document.querySelector("#result-date").textContent = respostaReq.date;
-      document.querySelector("#title").textContent = respostaReq.title;
-      document.querySelector("#result-img").src = respostaReq.hdurl;
-      document.querySelector("#explanation").textContent = respostaReq.explanation;
-    } else {
-      console.log(requisicao);
-    }
-  };
+// const btn = document.querySelector('#submit-btn');
 
-  requisicao.send();
-})
+// btn.addEventListener('click', function(event){
+//   event.preventDefault();
+//   requisicao.open("GET", url + dateInput.value + apiKey);
+
+//   requisicao.onload = function () {
+//     if (requisicao.status == 200) {
+//       const respostaReq = JSON.parse(requisicao.responseText);
+//       document.querySelector("#result-date").textContent = respostaReq.date;
+//       document.querySelector("#title").textContent = respostaReq.title;
+//       document.querySelector("#result-img").src = respostaReq.hdurl;
+//       document.querySelector("#explanation").textContent = respostaReq.explanation;
+//     } else {
+//       console.log(requisicao);
+//     }
+//   };
+
+//   requisicao.send();
+// })
 
